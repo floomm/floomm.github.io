fd06a58ffb994ca3efc362f868a2eb4a27e78b8d
Silvan Flum <54156690+floomm@users.noreply.github.com>
2024-10-05 18:08:18 +0200

    Add GitHub icon button

--- src/components/ActionMenu.tsx
+++ src/components/ActionMenu.tsx
@@ -1,5 +1,6 @@
-import { ColorMode, HStack, IconButton, Link } from "@chakra-ui/react";
+import {ColorMode, HStack, IconButton, Link} from "@chakra-ui/react";
 import { FaEnvelope, FaLinkedin, FaMoon, FaSun } from "react-icons/fa";
+import {FaGithub} from "react-icons/fa6";
 
 interface ActionMenuProps {
     colorMode: ColorMode;
@@ -10,29 +11,39 @@ const ActionMenu: React.FC<ActionMenuProps> = ({ colorMode, toggleColorMode }) =
     return (
         <HStack spacing={4}>
             <IconButton
-                aria-label="Toggle Dark/Light mode"
-                icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
-                size="md"
-                variant="ghost"
-                onClick={toggleColorMode}
-            />
-            <IconButton
-                aria-label="LinkedIn"
-                as={Link}
-                icon={<FaLinkedin />}
-                size="md"
-                variant="ghost"
-                href="https://www.linkedin.com/in/silvan-flum/"
-                target="_blank"
-                rel="noopener noreferrer"
-            />
-            <IconButton
                 aria-label="Email"
                 as={Link}
                 icon={<FaEnvelope />}
                 size="md"
                 variant="ghost"
                 href="mailto:contact@mail.flum.tech"
+            />
+            <IconButton
+                aria-label="LinkedIn Profile"
+                as={Link}
+                icon={<FaLinkedin />}
+                size="md"
+                variant="ghost"
+                href="https://www.linkedin.com/in/silvan-flum"
+                target="_blank"
+                rel="noopener noreferrer"
+            />
+            <IconButton
+                aria-label="GitHub Profile"
+                as={Link}
+                icon={<FaGithub />}
+                size="md"
+                variant="ghost"
+                href="https://www.github.com/floomm"
+                target="_blank"
+                rel="noopener noreferrer"
+            />
+            <IconButton
+                aria-label="Toggle Dark/Light mode"
+                icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+                size="md"
+                variant="ghost"
+                onClick={toggleColorMode}
             />
         </HStack>
     )
